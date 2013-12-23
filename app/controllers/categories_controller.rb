8d29ad5417040d6d66bf51545a355e56c9643cbb
Domikado 4 <domikado4@Domikado-4.local>
2013-12-23 18:12:23 +0700

    add method insert category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,19 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.create(category_params)
+    respond_to do |format|
+      if @category.save
+        format.html {redirect_to(category_params)}
+      end
+    end
   end
 
   def edit
@@ -16,4 +24,9 @@ class CategoriesController < ApplicationController
 
   def destroy
   end
+
+  private 
+    def category_params
+      params.require(:category).permit(:name, :picture_marker)
+    end
 end
